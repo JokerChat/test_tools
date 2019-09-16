656399ea6a89fbafffffbd3f9be76a0797e5b425
JokerChat <66461581@qq.com>
2019-09-16 17:50:32 +0800

    增加新项目

---
+++ public/common.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author       :junjie    
+# @Time         :2019/9/10 14:24
+# @FileName     :common.py
+#IDE            :PyCharm
+project_list=['mpm_key','mpwj_key','mvl_key']
+url=['push_url','mpm_url','mnl_url']
+sign=['push','mpm','mwj']
+db_name=['local','mpm','mwj']
