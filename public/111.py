c08e124eb854c7c505570c45369c077c758b3e9b
JokerChat <66461581@qq.com>
2020-07-29 11:34:40 +0800

    111

---
+++ public/111.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author       :junjie    
+# @Time         :2020/7/29 11:34
+# @FileName     :111.py
+# @Motto        :AS the tree,so the fruit
+#IDE            :PyCharm
+
+
+print(
+    111
+)
